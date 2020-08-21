29e7b75f108b383b7d07ff4923d82d5489ed6a34
Julien Ramel <julien.ramel@gmail.com>
2020-08-21 09:49:03 +0200

    fixed subscene

--- lib/subscene.rb
+++ lib/subscene.rb
@@ -46,7 +46,7 @@ module Subscene
   #     @attributes={:id=>"136037", :name=>"The.Big.Bang.Theory.."]
   #
   # Returns the Subtitles found.
-  def search(query=nil)
+  def self.search(query=nil)
     params = { q: query, l: '', r: true } unless query.nil?
     params ||= {}
 
@@ -69,13 +69,16 @@ module Subscene
   #   # => TODO: display example result
   #
   # Returns the complete information of the Subtitle.
-  def find(id)
+  def self.find(id)
     response = connection.get(id.to_s)
     html     = response.body
 
     subtitle = Subtitle.build(html)
     subtitle.id = id
     subtitle
+  end
+  def self.endpoint_url
+    ENDPOINT
   end
 
   # Public: Find a subtitle by id.
@@ -88,7 +91,7 @@ module Subscene
   #   # => TODO: display example result
   #
   # Returns the complete information of the Subtitle.
-  def findUrl(id, url)
+  def self.findUrl(id, url)
     params = {}
     response = connection.get do |req|
       req.url url, params
@@ -116,6 +119,22 @@ module Subscene
   def language=(lang_id)
     @lang_id = lang_id
   end
+  def language_name=(lang_name)
+    if lang_name.match(",")
+      lang_array=lang_name.split(",")
+      ret=[]
+      lang_array.each do |l|
+        ret<<languages_ids(l) unless languages_ids(l).nil?
+      end
+      @lang_id=ret.join(",")
+    else
+      @lang_id = languages_ids(lang_name)    
+    end
+  end
+  def self.lang_id
+    @lang_id
+  end
+
 
   private
 
@@ -126,5 +145,16 @@ module Subscene
       faraday.use      Subscene::Response::HTML
       faraday.use      Subscene::Response::RaiseError
     end
+  end
+
+
+  def languages_ids(name)
+    langs = { "Arabic" => 2 , "Brazillian Portuguese" => 4 , "Danish" => 10, "Dutch" => 11, "English" => 13, "Farsi/Persian" => 46, "Finnish" => 17, "French" => 18, "Greek" => 21, "Hebrew" => 22, "Indonesian" => 44, "Italian" => 26, "Korean" => 28, "Malay" => 50, "Norwegian" => 30, "Portuguese" => 32, "Romanian" => 33, "Spanish" => 38, "Swedish" => 39, "Turkish" => 41, "Vietnamese" => 45, "Albanian" => 1 , "Armenian" => 73, "Azerbaijani" => 55, "Basque" => 74, "Belarusian" => 68, "Bengali" => 54, "Big 5 code" => 3, "Bosnian" => 60, "Bulgarian" => 5 , "Bulgarian/ English" => 6 , "Burmese" => 61, "Cambodian/Khmer" => 79, "Catalan" => 49, "Chinese BG code" => 7 , "Croatian" => 8 , "Czech" => 9 , "Dutch/ English" => 12, "English/ German" => 15, "Esperanto" => 47, "Estonian" => 16, "Georgian" => 62, "German" => 19, "Greenlandic" => 57, "Hindi" => 51, "Hungarian" => 23, "Hungarian/ English" => 24, "Icelandic" => 25, "Japanese" => 27, "Kannada" => 78, "Kurdish" => 52, "Latvian" => 29, "Lithuanian" => 43, "Macedonian" => 48, "Malayalam" => 64, "Manipuri" => 65, "Mongolian" => 72, "Nepali" => 80, "Pashto" => 67, "Polish" => 31, "Punjabi" => 66, "Russian" => 34, "Serbian" => 35, "Sinhala" => 58, "Slovak" => 36, "Slovenian" => 37, "Somali" => 70, "Sundanese" => 76, "Swahili" => 75, "Tagalog" => 53, "Tamil" => 59, "Telugu" => 63, "Thai" => 40, "Ukrainian" => 56, "Urdu" => 42, "Yoruba" => 71 }
+    if langs.has_key?(name)
+      langs[name]
+    else
+      nil
+    end
+
   end
 end
